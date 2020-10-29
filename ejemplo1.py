3d87af3214d24fa449e076609432d5a5fbc46432
root <root@LAPTOP-3QGDNON6.localdomain>
2020-10-29 13:25:58 -0600

    Imagen

--- ejemplo1.py
+++ ejemplo1.py
@@ -12,6 +12,11 @@ imagen=cv2.imread("imagen.jpg")
 imagen=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 print(imagen.shape)
 print(imagen[0][0])
+
+imagen[0][0]=0
+imagen[0][1]=1
+imagen[0][2]=2
+
 cv2.imwrite("grayimagen.jpg",imagen)
 
 #cv2.imwrite("resizeimagen.jpg",imagen)
